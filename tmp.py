dc4b583a4d9df66c703642018f643246742dc23c
Meghan Smedes <127235615+megsmedes@users.noreply.github.com>
2023-06-23 17:29:23 +0000

    Max Ann Dis Bear Creek Bar graph

--- tmp.py
+++ tmp.py
@@ -6,8 +6,8 @@ import subprocess
 from io import BytesIO
 
 import folium
+import matplotlib
 import matplotlib.dates as dates
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 import requests
